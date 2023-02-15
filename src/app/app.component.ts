507b511cf892ae7b7b8835bfb9f6a7f5900c9c13
piyush-hack <75742731+piyush-hack@users.noreply.github.com>
2023-02-15 18:00:41 +0530

    ui fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {
   ChangeDetectorRef,
   Component,
+  Input,
   OnInit,
   Pipe,
   PipeTransform,
@@ -9,16 +10,16 @@ import {
 export class CalendarDay {
   public date: Date;
   public title: string;
-  public isPastDate: boolean;
+  public selectedMonthDate: boolean;
   public isToday: boolean;
 
   public getDateString() {
     return this.date.toISOString().split('T')[0];
   }
 
-  constructor(d: Date) {
+  constructor(d: Date, selectedMonthDate: boolean) {
     this.date = d;
-    this.isPastDate = d.setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0);
+    this.selectedMonthDate = selectedMonthDate || false;
     this.isToday = d.setHours(0, 0, 0, 0) == new Date().setHours(0, 0, 0, 0);
   }
 }
@@ -67,20 +68,14 @@ export class InCalenderRangePipe implements PipeTransform {
   }
 }
 
-// return (
-//   this.selectCalenderRange &&
-//   c.date.setHours(0, 0, 0, 0) >=
-//     this.selectedCalenderRange.startDay.date.setHours(0, 0, 0, 0) &&
-//   c.date.setHours(0, 0, 0, 0) <=
-//     this.selectedCalenderRange.endDay.date.setHours(0, 0, 0, 0)
-// );
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  @Input() selectCalenderRange: boolean = false;
+
   public calendar: CalendarDay[] = [];
   public monthNames = [
     'January',
@@ -98,28 +93,29 @@ export class AppComponent implements OnInit {
   ];
   public displayMonth: string;
   private monthIndex: number = 0;
-  selectedDay: any;
+  selectedDay: CalendarDay;
   selectedYear: number = new Date().getFullYear();
-  selectCalenderRange: boolean = false;
   selectedCalenderRange: CalendarRange;
 
   constructor(private cd: ChangeDetectorRef) {}
   ngOnInit(): void {
-    this.generateCalendarDays(this.monthIndex);
     if (!this.selectedCalenderRange) {
       this.selectedCalenderRange = new CalendarRange(null, null);
     }
+
+    this.generateCalendarDays();
   }
 
-  private generateCalendarDays(monthIndex: number): void {
+  private generateCalendarDays(monthIndex: number = null): void {
     // we reset our calendar
     this.calendar = [];
 
     // we set the date
     let day: Date = new Date();
-    day.setMonth(new Date().getMonth() + monthIndex);
+    let showCalenderFromMonth = this.getStartMonthForCalender(monthIndex);
+    day.setMonth(showCalenderFromMonth);
     day.setFullYear(this.selectedYear);
-
+    console.log(this.selectedYear);
     // set the dispaly month for UI
     this.displayMonth = this.monthNames[day.getMonth()];
 
@@ -128,9 +124,30 @@ export class AppComponent implements OnInit {
     let dateToAdd = startingDateOfCalendar;
 
     for (var i = 0; i < 42; i++) {
-      this.calendar.push(new CalendarDay(new Date(dateToAdd)));
+      let selectedMonthDate = true;
+      if (dateToAdd.getMonth() !== showCalenderFromMonth) {
+        selectedMonthDate = false;
+      }
+      this.calendar.push(
+        new CalendarDay(new Date(dateToAdd), selectedMonthDate)
+      );
       dateToAdd = new Date(dateToAdd.setDate(dateToAdd.getDate() + 1));
     }
+  }
+
+  private getStartMonthForCalender(monthIndex: number = null) {
+    const userSelectedDay =
+      this.selectedDay && this.selectedDay.date.getMonth();
+    const userSelectedRange = this.selectCalenderRange
+      ? this.selectedCalenderRange &&
+        this.selectedCalenderRange.startDay &&
+        this.selectedCalenderRange.startDay.date.getMonth()
+      : null;
+      this.monthIndex = monthIndex ??
+      userSelectedDay ??
+      userSelectedRange ??
+      new Date().getMonth()
+    return this.monthIndex;
   }
 
   private getStartDateForCalendar(selectedDate: Date) {
@@ -155,21 +172,49 @@ export class AppComponent implements OnInit {
 
   public increaseMonth() {
     this.monthIndex++;
+    this.onMonthIndexIncDec();
     this.generateCalendarDays(this.monthIndex);
   }
 
   public decreaseMonth() {
     this.monthIndex--;
+    this.onMonthIndexIncDec();
     this.generateCalendarDays(this.monthIndex);
+  }
+
+  private onMonthIndexIncDec() {
+    if (this.monthIndex > 11) {
+      this.monthIndex = 0;
+      this.selectedYear++;
+    }
+
+    if (this.monthIndex < 0) {
+      this.monthIndex = 11;
+      this.selectedYear--;
+    }
   }
 
   public setCurrentMonth() {
-    this.monthIndex = 0;
+    this.monthIndex = new Date().getMonth();
+    this.selectedYear = new Date().getFullYear(); 
     this.generateCalendarDays(this.monthIndex);
   }
 
-  public getSelectedDate(selectedDay: CalendarDay) {
+  public clearCalendar() {
+    this.selectedDay = null;
+    this.selectedCalenderRange = null;
+  }
+
+  public selectDate(selectedDay: CalendarDay) {
+    console.log(selectedDay , selectedDay.date.getMonth())
+    if (!selectedDay.selectedMonthDate) {
+      this.monthIndex = selectedDay.date.getMonth();
+      this.selectedYear = selectedDay.date.getFullYear();
+      this.generateCalendarDays(this.monthIndex);
+    }
+ 
     if (this.selectCalenderRange) {
+      this.selectedDay = null;
       this.setCalendarRange(selectedDay);
     } else {
       this.selectedCalenderRange = null;
@@ -182,23 +227,30 @@ export class AppComponent implements OnInit {
       this.selectedCalenderRange = new CalendarRange(null, null);
     }
 
-    if (!this.selectedCalenderRange.startDay) {
+    if (this.rangeAlreadyExists() && this.notSelectedRangeEnds(selectedDay)) {
+      this.removeNearestSelectedRangeEnds(selectedDay);
+    }
+
+    if (
+      !this.selectedCalenderRange.startDay &&
+      !this.isSelectedRangeEndDay(selectedDay)
+    ) {
       this.selectedCalenderRange.startDay = selectedDay;
       return;
     }
 
-    if (
-      this.selectedCalenderRange.startDay.date.setHours(0, 0, 0, 0) ===
-      selectedDay.date.setHours(0, 0, 0, 0)
-    ) {
+    if (this.isSelectedRangeStartDay(selectedDay)) {
       this.selectedCalenderRange.startDay = null;
-
+      if (this.selectedCalenderRange.endDay) {
+        this.selectedCalenderRange.startDay = this.selectedCalenderRange.endDay;
+        this.selectedCalenderRange.endDay = null;
+      }
       return;
     }
 
     if (
-      this.selectedCalenderRange.startDay.date.setHours(0, 0, 0, 0) >
-      selectedDay.date.setHours(0, 0, 0, 0)
+      !this.selectedCalenderRange.endDay &&
+      this.isBeforeSelectedRangeStartDay(selectedDay)
     ) {
       this.selectedCalenderRange.endDay = this.selectedCalenderRange.startDay;
       this.selectedCalenderRange.startDay = selectedDay;
@@ -210,13 +262,73 @@ export class AppComponent implements OnInit {
       return;
     }
 
-    if (
-      this.selectedCalenderRange.endDay.date.setHours(0, 0, 0, 0) ===
-      selectedDay.date.setHours(0, 0, 0, 0)
-    ) {
+    if (this.isSelectedRangeEndDay(selectedDay)) {
       this.selectedCalenderRange.endDay = null;
       return;
     }
+  }
+
+  private rangeAlreadyExists() {
+    return (
+      this.selectedCalenderRange.startDay && this.selectedCalenderRange.endDay
+    );
+  }
+  private notSelectedRangeEnds(selectedDay: CalendarDay) {
+    return (
+      this.rangeAlreadyExists() &&
+      this.selectedCalenderRange.startDay.date.setHours(0, 0, 0, 0) !==
+        selectedDay.date.setHours(0, 0, 0, 0) &&
+      this.selectedCalenderRange.endDay.date.setHours(0, 0, 0, 0) !==
+        selectedDay.date.setHours(0, 0, 0, 0)
+    );
+  }
+
+  private removeNearestSelectedRangeEnds(selectedDay: CalendarDay) {
+    const rangeStartDay = this.selectedCalenderRange.startDay.date;
+    const rangeEndDay = this.selectedCalenderRange.endDay.date;
+    const currentSelectedDay = selectedDay.date;
+
+    // Calculate differences in milliseconds
+    const fromStartDay = currentSelectedDay.getTime() - rangeStartDay.getTime();
+    const fromEndDay = currentSelectedDay.getTime() - rangeEndDay.getTime();
+
+    // Convert differences to days
+    let daysFromStartDay = fromStartDay / (1000 * 60 * 60 * 24);
+    let daysFromEndDay = fromEndDay / (1000 * 60 * 60 * 24);
+
+    // Compare differences
+    if (daysFromStartDay > 0 && daysFromEndDay < 0) {
+      this.selectedCalenderRange.endDay = null;
+    } else {
+      daysFromStartDay = Math.abs(daysFromStartDay);
+      daysFromEndDay = Math.abs(daysFromEndDay);
+      if (daysFromStartDay < daysFromEndDay) {
+        this.selectedCalenderRange.startDay = null;
+      } else {
+        this.selectedCalenderRange.endDay = null;
+      }
+    }
+  }
+
+  private isBeforeSelectedRangeStartDay(selectedDay: CalendarDay) {
+    return (
+      this.selectedCalenderRange.startDay.date.setHours(0, 0, 0, 0) >
+      selectedDay.date.setHours(0, 0, 0, 0)
+    );
+  }
+
+  private isSelectedRangeStartDay(selectedDay: CalendarDay) {
+    return (
+      this.selectedCalenderRange?.startDay?.date?.setHours(0, 0, 0, 0) ===
+      selectedDay.date.setHours(0, 0, 0, 0)
+    );
+  }
+
+  private isSelectedRangeEndDay(selectedDay: CalendarDay) {
+    return (
+      this.selectedCalenderRange?.endDay?.date?.setHours(0, 0, 0, 0) ===
+      selectedDay.date.setHours(0, 0, 0, 0)
+    );
   }
 
   private compareDates(d: Date, e: Date) {
